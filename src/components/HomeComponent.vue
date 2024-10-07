b21e98a5f0267da57522645b6a1823690436016e
Sovandy Khgney <sovandykh168@gmail.com>
2024-10-07 18:30:12 +0700

    update_home

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -5,5 +5,6 @@ const count = ref(0)
 </script>
 
 <template>
+  <h1 class="text-center">Hello World</h1>
   <button class="btn btn-primary" @click="count++">You clicked me {{ count }} times.</button>
 </template>
